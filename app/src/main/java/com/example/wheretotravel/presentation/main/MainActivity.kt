534472f736994bf19fdcfc852094712aa0ad66d0
Daniil Fatykov <fatikoff2002@mail.ru>
2022-05-17 23:59:56 +0300

    add nav

--- app/src/main/java/com/example/wheretotravel/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/wheretotravel/presentation/main/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.wheretotravel.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.wheretotravel.R
 import com.example.wheretotravel.databinding.ActivityMainBinding
 import com.example.wheretotravel.presentation.main.fragment.HelpFragment
 import com.example.wheretotravel.presentation.main.fragment.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 //import com.example.wheretotravel.domain.usecase.GetUserNameUseCase
 //import com.example.wheretotravel.domain.usecase.SaveUserNameUseCase
@@ -16,17 +21,21 @@ class MainActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             binding  = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
+            val navView:BottomNavigationView = findViewById(R.id.nav_view)
 
-            binding.btnFragHelp.setOnClickListener {
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.place_holder, HelpFragment.newInstance())
-                    .commit()
-            }
+            val navController = findNavController(R.id.nav_host_fragment)
+            val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.search_fragment,
+                R.id.help_fragment,
+                R.id.ride_fragment,
+                R.id.profile_fragment))
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
 
-            supportFragmentManager
+
+            /*supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.place_holder, SearchFragment.newInstance())
-                .commit()
+                .commit()*/
         }
 }
